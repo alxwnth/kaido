5c9f7b7e2654897e5174ee803e6432aa348ad7ce
alxwnth <alxwnth@posteo.me>
2024-04-14 10:00:49 +0300

    Reinitialize project with Tauri v2 + Svelte + Tailwind

---
+++ src-tauri/gen/android/app/src/main/java/com/tauri/kaido/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.tauri.kaido
+
+class MainActivity : TauriActivity()
--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-
-import "./styles.scss";
-import '/node_modules/modern-normalize/modern-normalize.css';
-
-import App from "./App";
-
-render(() => <App />, document.getElementById("root"));
